ec116f8d1a3940159206027dd45c77a6f7ba1450
Justin Yu <justinxyu1128@gmail.com>
2021-03-26 23:53:51 -0700

    created for loop that checks for complete todos and adds to completedTodos array

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,7 @@
 let todoList = [{todo: "clone repo for starter code", status:"complete",}, {todo: "create array todoList", status:"complete"}, {todo: "create array completedTodos", status:"started"}, {todo: "create for loops", status:"started"}];
 let completedTodos = [];
+for (let i of todoList) {
+    if (i.status === "complete") {
+        completedTodos.push(i);
+    }
+}
